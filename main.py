d54e35471dd5bc9f87e6f56b03322ef41f6622ed
Nahuel Coronel <nahucarc2@gmail.com>
2023-08-03 20:57:14 -0300

    Falta parte 3 con busqueda greedy

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ class Objeto:
     def __str__(self):
         return f"Objeto<valor: {self.valor} -- volumen: {self.volumen}>"
 
+    def get_proporcion(self):
+        return self.valor / self.volumen
 
 
 objetos = [
@@ -45,6 +47,8 @@ objetos = [
 
 
 
+#PARTE 1
+
 combinaciones:[] = []
 for r in range(1, len(objetos) + 1):
     for comb in combinations(objetos, r):
@@ -61,5 +65,74 @@ for comb in combinaciones:
         valor_max=valor_total
         comb_max =  comb
 
+volumen = sum(obj.volumen for obj in comb_max)
 
-print(f"combinacion maxima: {comb_max} -- valor: {valor_max}")
+print(f"combinacion maxima: {comb_max} -- valor: {valor_max} -- volumen: {volumen}")
+
+
+
+#PARTE 2
+mochila = []
+objetos_ordenados = sorted(objetos, key= lambda obj:obj.get_proporcion() , reverse=True)
+
+for obj in objetos_ordenados:
+    total_volumen_mochila = sum(obj.volumen for obj in mochila)
+
+    if total_volumen_mochila + obj.volumen < VOLUMEN_MAXIMO:
+        mochila.append(obj)
+
+
+valor_mochila = sum(obj.valor for obj in mochila)
+volumen_mochila = sum(obj.volumen for obj in mochila)
+print(f"mochila:{mochila} -- valor: {valor_mochila} -- volumen: {volumen_mochila}")
+
+
+# PARTE 3
+@dataclass
+class Objeto:
+    _peso: int
+    _valor: int
+
+    @property
+    def peso(self):
+        return self._peso
+
+    @property
+    def valor(self):
+        return self._valor
+
+    @peso.setter
+    def peso(self, peso):
+        self._peso = peso
+
+    @valor.setter
+    def valor(self, valor):
+        self._valor = valor
+
+    def __str__(self):
+        return f"Objeto<valor: {self.valor} -- volumen: {self._peso}>"
+
+
+objetos = [
+    Objeto(1800, 72),
+    Objeto(600, 36),
+    Objeto(1200, 60)
+
+]
+
+combinaciones: [] = []
+for r in range(1, len(objetos) + 1):
+    for comb in combinations(objetos, r):
+        peso_total = sum(obj.peso for obj in comb)
+        if peso_total <= PESO_MAXIMO:
+            combinaciones.append(comb)
+
+valor_max = 0
+comb_max = combinaciones[0]
+for comb in combinaciones:
+    valor_total = sum(obj.valor for obj in comb)
+    if valor_total > valor_max:
+        valor_max = valor_total
+        comb_max = comb
+
+
